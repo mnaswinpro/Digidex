7a0ed2d6c281dfac030bdf359fcef8ebeb8bbedb
Aswin MN <mnaswinpro@gmail.com>
2023-10-14 11:14:06 +0530

    added parcelize for parcelable objects

--- app/src/main/java/com/digidex/domain/data/Digimon.kt
+++ app/src/main/java/com/digidex/domain/data/Digimon.kt
@@ -1,8 +1,12 @@
 package com.digidex.domain.data
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class Digimon(
     val id: String,
     val name: String,
     val imageUrl: String,
     val detailUrl: String
-)
+) : Parcelable
