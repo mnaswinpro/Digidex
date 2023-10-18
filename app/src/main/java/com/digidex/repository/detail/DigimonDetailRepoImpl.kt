7caa1be77b2c405e2c3f08cad8312595ad29d98f
Aswin MN <mnaswinpro@gmail.com>
2023-10-18 12:43:06 +0530

    updated repository impl to remove retrofit callbacks and use coroutines

--- app/src/main/java/com/digidex/repository/detail/DigimonDetailRepoImpl.kt
+++ app/src/main/java/com/digidex/repository/detail/DigimonDetailRepoImpl.kt
@@ -1,41 +1,30 @@
 package com.digidex.repository.detail
 
+import com.digidex.dispatcher.CoroutineDispatcherApi
 import com.digidex.repository.DigimonApi
 import com.digidex.repository.NetworkResult
+import com.digidex.repository.NoDataException
 import com.digidex.repository.detail.data.DigimonDetailResponse
-import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.callbackFlow
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import java.lang.Exception
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.withContext
+import retrofit2.await
 import javax.inject.Inject
 
-class DigimonDetailRepoImpl @Inject constructor(private val api: DigimonApi) : DigimonDetailRepo {
-
-    override suspend fun execute(url: String): Flow<NetworkResult<DigimonDetailResponse>> = callbackFlow {
-        val callback = object : Callback<DigimonDetailResponse> {
-            override fun onResponse(
-                call: Call<DigimonDetailResponse>,
-                response: Response<DigimonDetailResponse>
-            ) {
-                val result = response.body()
+class DigimonDetailRepoImpl @Inject constructor(
+    private val api: DigimonApi,
+    private val dispatcher: CoroutineDispatcherApi
+) : DigimonDetailRepo {
+    override suspend fun execute(url: String): Flow<NetworkResult<DigimonDetailResponse>> {
+        return withContext(dispatcher.io) {
+            flow {
+                val result = api.getDigimon(url)?.await()
                 if (result == null) {
-                    trySend(NetworkResult.DataError)
+                    emit(NetworkResult.Error(NoDataException()))
                 } else {
-                    trySend(NetworkResult.Success(result))
+                    emit(NetworkResult.Success(result))
                 }
             }
-
-            override fun onFailure(call: Call<DigimonDetailResponse>, t: Throwable) {
-                trySend(NetworkResult.GenericError(Exception(t)))
-            }
-
-        }
-        with(api.getDigimon(url)) {
-            this?.enqueue(callback)
-            awaitClose { this?.cancel() }
         }
     }
 }
--- app/src/main/java/com/digidex/repository/listing/DigimonListingRepoImpl.kt
+++ app/src/main/java/com/digidex/repository/listing/DigimonListingRepoImpl.kt
@@ -1,41 +1,30 @@
 package com.digidex.repository.listing
 
+import com.digidex.dispatcher.CoroutineDispatcherApi
 import com.digidex.repository.DigimonApi
 import com.digidex.repository.NetworkResult
+import com.digidex.repository.NoDataException
 import com.digidex.repository.listing.data.DigimonListResponse
-import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.callbackFlow
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import java.lang.Exception
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.withContext
+import retrofit2.await
 import javax.inject.Inject
 
-class DigimonListingRepoImpl @Inject constructor(private val api: DigimonApi) : DigimonListingRepo {
-
-    override suspend fun execute(): Flow<NetworkResult<DigimonListResponse>> = callbackFlow {
-        val callback = object : Callback<DigimonListResponse> {
-            override fun onResponse(
-                call: Call<DigimonListResponse>,
-                response: Response<DigimonListResponse>
-            ) {
-                val result = response.body()
+class DigimonListingRepoImpl @Inject constructor(
+    private val api: DigimonApi,
+    private val dispatcher: CoroutineDispatcherApi
+) : DigimonListingRepo {
+    override suspend fun execute(): Flow<NetworkResult<DigimonListResponse>> {
+        return withContext(dispatcher.io) {
+            flow {
+                val result = api.getDigimonList()?.await()
                 if (result == null) {
-                    trySend(NetworkResult.DataError)
+                    emit(NetworkResult.Error(NoDataException()))
                 } else {
-                    trySend(NetworkResult.Success(result))
+                    emit(NetworkResult.Success(result))
                 }
             }
-
-            override fun onFailure(call: Call<DigimonListResponse>, t: Throwable) {
-                trySend(NetworkResult.GenericError(Exception(t)))
-            }
-
-        }
-        with(api.getDigimonList()) {
-            this?.enqueue(callback)
-            awaitClose { this?.cancel() }
         }
     }
 }
