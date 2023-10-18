c540cf8b3a2edb54c5a8ba4f0dec2ea8e084a96f
Aswin MN <mnaswinpro@gmail.com>
2023-10-18 14:21:45 +0530

    updated message of custom exception

--- app/src/main/java/com/digidex/repository/NetworkException.kt
+++ app/src/main/java/com/digidex/repository/NetworkException.kt
@@ -3,4 +3,6 @@ package com.digidex.repository
 /**
  * A custom exception class when response data is null
  */
-class NoDataException : Exception()
+class NoDataException : Exception() {
+    override val message : String = "No data found"
+}
