7996d6c2b6866c154a3c072793a1518996d78314
Aswin MN <mnaswinpro@gmail.com>
2023-10-18 14:20:48 +0530

    formatted basefrag

--- app/src/main/java/com/digidex/base/BaseFragment.kt
+++ app/src/main/java/com/digidex/base/BaseFragment.kt
@@ -8,8 +8,10 @@ import androidx.fragment.app.Fragment
 open class BaseFragment : Fragment() {
 
     protected fun isNetworkAvailable(): Boolean {
-        val connectivityManager = context?.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        val networkCapabilities = connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+        val connectivityManager =
+            context?.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val networkCapabilities =
+            connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
         return networkCapabilities?.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) == true
     }
 }
